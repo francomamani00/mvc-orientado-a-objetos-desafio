aa9131a592f2b4fe707555a3a824269ec7463d47
unknown <franco.mamani00@gmail.com>
2021-08-11 02:31:34 -0300

    primer commit

--- models.ts
+++ models.ts
@@ -1,7 +1,34 @@
+import * as jsonfile from "jsonfile";
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  arrayDeLaData: Contact[] = [];
+  load() {
+    const arrayDeContacts = jsonfile.readFyleSinc("./contacts.json");
+    this.arrayDeLaData = arrayDeContacts;
+  }
+  // lee el archivo de contacts.json y lo guarda
+  // en arrayDeLaData
+  getAll() {
+    return this.arrayDeLaData;
+  }
+  addOne(contact: Contact) {
+    return this.arrayDeLaData.push(contact);
+  }
+  save() {
+    jsonfile.writeFyleSync("./contacts.json", this.arrayDeLaData);
+    // esto hace que el contacts.json se actualice cada vez
+    // que agregamos a algun contacto con el metodo addOne
+    // ESTO REESCRIBE EL CONTACTS.JSON
+  }
+  getOneById(id) {
+    let buscando = this.arrayDeLaData.find(function (p) {
+      return p.id == id;
+    });
+    return buscando;
+  }
+}
 export { ContactsCollection };
